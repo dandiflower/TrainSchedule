e64c8d3a5d071c2938c46be9cedfb427f544aad7
Jessica Brush <jessicadawnbrush@gmail.com>
2018-05-12 11:21:54 -0700

    push not syncing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,14 +20,14 @@ firebase.initializeApp(config);
 // --------------------------------------------------------------
 // Link to Firebase Database for viewer tracking
 var database = firebase.database();
-window.onload = function () {
+// window.onload = function () {
 
-// initial values
-var tName = "";
-var destination = "";
-var militaryTime = "";
-var freq = "";
-var nextTrain = "";
+// // initial values
+// var tName = "";
+// var destination = "";
+// var militaryTime = "";
+// var freq = "";
+// var nextTrain = "";
 
 // --------------------------------------------------------------
 // Dynamically update table entry with user information
@@ -35,39 +35,21 @@ var nextTrain = "";
     $(document).on("click", ".btn", function () {
         event.preventDefault();
 
+        var tName = $("#trainName").val().trim();
+        var destination = $("#destination").val().trim();
+        var militaryTime = $("#starttime").val().trim();
+        var freq = $("#freq").val().trim();
+ 
+        // First Time (pushed back 1 year to make sure it comes before current time)
+        var firstTimeConverted = moment(militaryTime, "HH:mm").subtract(1, "years");
+        console.log(firstTimeConverted);
 
-
-            // table value input
-            var table = document.getElementById("myTable");
-            var row = table.insertRow(1);
-            var cell1 = row.insertCell(0);
-            var cell2 = row.insertCell(1);
-            var cell3 = row.insertCell(2);
-            var cell4 = row.insertCell(3);
-            var cell5 = row.insertCell(4);
-            cell1.innerHTML = tName;
-            cell2.innerHTML = destination;
-            cell3.innerHTML = freq;
-            cell4.innerHTML = nextTrain;
-            cell5.innerHTML = tMinutesTillTrain;
-            //    console.log(tName);
-            //    console.log(destination);
-
-  
-        // grab values form textboxes 
-        tName = $("#trainName").val().trim();
-        destination = $("#destination").val().trim();
-        militaryTime = $("#starttime").val().trim();
-        // console.log(militaryTime);
-        freq = $("#freq").val().trim();
-        // console.log(freq);
-        
         // Current Time
         var currentTime = moment();
         console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
         // Difference between the times
-        var diffTime = moment().diff(moment(militaryTime), "minutes");
+        var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
         console.log("DIFFERENCE IN TIME: " + diffTime);
 
         // Time apart (remainder)
@@ -78,20 +60,58 @@ var nextTrain = "";
         var tMinutesTillTrain = freq - tRemainder;
         console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
-        // nextTrain = moment().add(tMinutesTillTrain, "minutes");
-        // console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+        var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+        console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
-            // Code for handling the push
-            database.ref().push({
-                tName: tName,
-                destination: destination,
-                militaryTime: militaryTime,
-                freq: freq,
-                nextTrain: nextTrain,
-            });
+        // Creates local "temporary" object for holding employee data
+        var train = {
+            tName: tName,
+            destination: destination,
+            militaryTime: militaryTime,
+            nextTrain: nextTrain,
+            freq: freq,
+        };
 
-        });
-};
+            
+
+    // Code for handling the push
+    database.ref().push(train);
+
+    console.log(train.tName);
+    console.log(train.destination);
+    console.log(train.militaryTime);
+    console.log(train.nextTrain);
+    console.log(train.freq);
+});
+
+// database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+//         // grab values from textboxes 
+//         tName = $("#trainName").val().trim();
+//         destination = $("#destination").val().trim();
+//         militaryTime = $("#starttime").val().trim();
+//         console.log(militaryTime);
+//         freq = $("#freq").val().trim();
+//         console.log(freq);
+
+//         // table value input from DOM
+//         var table = document.getElementById("myTable");
+//         var row = table.insertRow(1);
+//         var cell1 = row.insertCell(0);
+//         var cell2 = row.insertCell(1);
+//         var cell3 = row.insertCell(2);
+//         // var cell4 = row.insertCell(3);
+//         // var cell5 = row.insertCell(4);
+//         cell1.innerHTML = tName;
+//         cell2.innerHTML = destination;
+//         cell3.innerHTML = freq;
+//         // cell4.innerHTML = nextTrain;
+//         // cell5.innerHTML = tMinutesTillTrain;
+//         //    console.log(tName);
+//         //    console.log(destination);
+
+//         });
+
 
 
 
