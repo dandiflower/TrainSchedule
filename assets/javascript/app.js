d5fc05ef4fb8352d6c84ee700cca2212c5f84dd3
Jessica Brush <jessicadawnbrush@gmail.com>
2018-05-11 23:34:44 -0700

    time conversion v1

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,46 +25,61 @@ window.onload = function () {
 // initial values
 var tName = "";
 var destination = "";
-var militaryTime = 0;
-var freq = 0;
-
+var militaryTime = "";
+var freq = "";
+var nextTrain = "";
 
 // --------------------------------------------------------------
 // Dynamically update table entry with user information
 
     $(document).on("click", ".btn", function () {
         event.preventDefault();
-        // Initial Values
-        // var frequency = initial;
-        // var nextArrival = initial;
-        // var minAway = tbd;
 
-        tName = $("#trainName").val().trim();
-        destination = $("#destination").val().trim();
+
 
             // table value input
             var table = document.getElementById("myTable");
             var row = table.insertRow(1);
             var cell1 = row.insertCell(0);
             var cell2 = row.insertCell(1);
-            // var cell3 = row.insertCell(2);
-            // var cell4 = row.insertCell(3);
-            // var cell5 = row.insertCell(4);
+            var cell3 = row.insertCell(2);
+            var cell4 = row.insertCell(3);
+            var cell5 = row.insertCell(4);
             cell1.innerHTML = tName;
             cell2.innerHTML = destination;
-            // cell3.innerHTML = frequency;
-            // cell4.innerHTML = nextArrival;
-            // cell5.innerHTML = minutesAway;
+            cell3.innerHTML = freq;
+            cell4.innerHTML = nextTrain;
+            cell5.innerHTML = tMinutesTillTrain;
             //    console.log(tName);
             //    console.log(destination);
 
   
-        // grab values form textboxes
+        // grab values form textboxes 
+        tName = $("#trainName").val().trim();
+        destination = $("#destination").val().trim();
         militaryTime = $("#starttime").val().trim();
-        console.log(militaryTime);
+        // console.log(militaryTime);
         freq = $("#freq").val().trim();
-        console.log(freq);
+        // console.log(freq);
+        
+        // Current Time
+        var currentTime = moment();
+        console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
+        // Difference between the times
+        var diffTime = moment().diff(moment(militaryTime), "minutes");
+        console.log("DIFFERENCE IN TIME: " + diffTime);
+
+        // Time apart (remainder)
+        var tRemainder = diffTime % freq;
+        console.log(tRemainder);
+
+        // Minute Until Train
+        var tMinutesTillTrain = freq - tRemainder;
+        console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+        nextTrain = moment().add(tMinutesTillTrain, "minutes");
+        console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
             // Code for handling the push
             database.ref().push({
@@ -72,10 +87,74 @@ var freq = 0;
                 destination: destination,
                 militaryTime: militaryTime,
                 freq: freq,
+                nextTrain: nextTrain,
             });
 
         });
 
+
+    // Assume the following situations.
+
+    // (TEST 1)
+    // First Train of the Day is 3:00 AM
+    // Assume Train comes every 3 minutes.
+    // Assume the current time is 3:16 AM....
+    // What time would the next train be...? (Use your brain first)
+    // It would be 3:18 -- 2 minutes away
+
+    // (TEST 2)
+    // First Train of the Day is 3:00 AM
+    // Assume Train comes every 7 minutes.
+    // Assume the current time is 3:16 AM....
+    // What time would the next train be...? (Use your brain first)
+    // It would be 3:21 -- 5 minutes away
+
+
+    // ==========================================================
+
+    // Solved Mathematically
+    // Test case 1:
+    // 16 - 00 = 16
+    // 16 % 3 = 1 (Modulus is the remainder)
+    // 3 - 1 = 2 minutes away
+    // 2 + 3:16 = 3:18
+
+    // Solved Mathematically
+    // Test case 2:
+    // 16 - 00 = 16
+    // 16 % 7 = 2 (Modulus is the remainder)
+    // 7 - 2 = 5 minutes away
+    // 5 + 3:16 = 3:21
+
+    // Assumptions
+    // var tFrequency = 3;
+
+    // Time is 3:30 AM
+    // var firstTime = "03:30";
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    // var firstTimeConverted = moment(militaryTime, "HH:mm");
+    // console.log(firstTimeConverted);
+
+    // // Current Time
+    // var currentTime = moment();
+    // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    // // Difference between the times
+    // var diffTime = moment().diff(moment(militaryTime), "minutes");
+    // console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // // Time apart (remainder)
+    // var tRemainder = diffTime % freq;
+    // console.log(tRemainder);
+
+    // // Minute Until Train
+    // var tMinutesTillTrain = freq - tRemainder;
+    // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // // Next Train
+    // var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    // console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 // ----------------------------------------------------------------
 // At the page load and subsequent value changes, get a snapshot of the local data.
 // This function allows you to update your page in real-time when the values within the firebase node bidderData changes
